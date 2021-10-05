21359b8ea02fe9ef0eaddd826e5d5b38e1a01d7a
albert.greinoecker@gmail.com <albert.greinoecker@gmail.com>
2021-10-05 18:30:51 +0200

    we don't want emp_no in the model

--- jpa-employees/src/main/java/at/ac/htlinn/jpa/employees/Title.java
+++ jpa-employees/src/main/java/at/ac/htlinn/jpa/employees/Title.java
@@ -2,8 +2,6 @@ package at.ac.htlinn.jpa.employees;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -12,29 +10,19 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "titles")
 public class Title {
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long emp_no;
-	
-	
 	@Column(name = "from_date")
 	private String fromDate;
 	@Column(name = "to_date")
 	private String toDate;
 
+	@Id
 	@ManyToOne
-	@JoinColumn(name = "emp_no", nullable = false, insertable=false, updatable=false)
+	@JoinColumn(name = "emp_no")
 	private Employee employee;
-	
+
 	public Title() {
-	}
-
-	public long getEmp_no() {
-		return emp_no;
-	}
-
-	public void setEmp_no(long emp_no) {
-		this.emp_no = emp_no;
 	}
 
 	public String getFromDate() {
@@ -63,9 +51,8 @@ public class Title {
 
 	@Override
 	public String toString() {
-		return "Title [emp_no=" + emp_no + ", fromDate=" + fromDate + ", toDate=" + toDate + ", employee=" + employee
+		return "Title [emp_no=" + employee + ", fromDate=" + fromDate + ", toDate=" + toDate + ", employee=" + employee
 				+ "]";
 	}
-	
-	
+
 }
