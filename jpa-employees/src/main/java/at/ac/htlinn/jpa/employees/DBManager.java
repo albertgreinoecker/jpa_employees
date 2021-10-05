6c8419c44a0c2202997535c147b98e6ea3a97109
albert.greinoecker@gmail.com <albert.greinoecker@gmail.com>
2021-10-05 18:28:29 +0200

    improved presentation of the samples

--- jpa-employees/src/main/java/at/ac/htlinn/jpa/employees/DBManager.java
+++ jpa-employees/src/main/java/at/ac/htlinn/jpa/employees/DBManager.java
@@ -1,5 +1,8 @@
 package at.ac.htlinn.jpa.employees;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -9,24 +12,24 @@ import javax.persistence.Persistence;
 import javax.persistence.Query;
 
 public class DBManager {
-	//Ist ein wenig wie die Connection/Connection Pool bei JDBC
+	// Ist ein wenig wie die Connection/Connection Pool bei JDBC
 	private EntityManagerFactory emf;
 
 	public DBManager() {
 		emf = Persistence.createEntityManagerFactory("employees");
-		
+
 	}
 
-	
-	public Department getDepartment(String deptNo)
-	{
+	public Department getDepartment(String deptNo) {
 		EntityManager em = emf.createEntityManager();
 		Department professor = em.find(Department.class, deptNo);
 		em.close();
 		return professor;
 	}
-	
-	
+
+	/**
+	 * List all employees (limit set to 20)
+	 */
 	public List<Employee> getEmployees() {
 		EntityManager em = emf.createEntityManager();
 		// JPQL - Java Persistence Query Language - nicht SQL
@@ -38,10 +41,12 @@ public class DBManager {
 		em.close();
 		return employee;
 	}
-	
-	public List<Salary> getSalaries(){
+
+	/**
+	 * Get all Salary entries. Does not make sense, just for demonstration purposes
+	 */
+	public List<Salary> getSalaries() {
 		EntityManager em = emf.createEntityManager();
-		// JPQL - Java Persistence Query Language - nicht SQL
 		String sql = "SELECT s From Salary s";
 		Query query = em.createQuery(sql, Employee.class);
 		query.setMaxResults(20);
@@ -50,80 +55,88 @@ public class DBManager {
 		em.close();
 		return salaries;
 	}
-	
+
 	public Title getTitle(long empno) {
 		EntityManager em = emf.createEntityManager();
 		Title title = em.find(Title.class, empno);
 		em.close();
 		return title;
 	}
-	
+
 	public Employee getEmployee(int empno) {
 		EntityManager em = emf.createEntityManager();
 		Employee employee = em.find(Employee.class, empno);
 		em.close();
 		return employee;
 	}
-	
-	public Salary getSalary(long empno) {
+
+	public Salary getActualSalary(long empno) {
+		Date d9999 = null;
+		try {
+			d9999 = new SimpleDateFormat("yyyyMMdd").parse("99990101");
+		} catch (ParseException e1) {
+			e1.printStackTrace();
+		}
+		return getSalary(empno, d9999);
+	}
+
+	public Salary getSalary(long empno, Date d) {
+
 		EntityManager em = emf.createEntityManager();
-		// JPQL - Java Persistence Query Language - nicht SQL
-		String sql = "SELECT s FROM Salary s WHERE s.emp_no = :empNo";
+		String sql = "SELECT s FROM Salary s WHERE s.emp_no = :empNo AND s.toDate = :date";
 		Query query = em.createQuery(sql, Employee.class);
 		query.setParameter("empNo", empno);
+		System.out.println(d);
+		query.setParameter("date", d);
 		Salary salary = null;
-		try
-		{
-			salary = (Salary)query.getSingleResult();
-		} catch (NoResultException e)
-		{
+		try {
+			salary = (Salary) query.getSingleResult();
+		} catch (NoResultException e) {
 			e.printStackTrace();
 		}
 		em.close();
 		return salary;
 	}
-	
-	public void removeEmployee(int matrNr)
-	{
-		EntityManager em = emf.createEntityManager(); 
-		Employee employee = getEmployee(matrNr); 
-		em.remove(employee);
+
+	public void removeEmployee(int persNr) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Employee employee = getEmployee(persNr);
+		if (employee != null) {
+			employee = em.merge(employee);
+			em.remove(employee);
+			em.getTransaction().commit();
+		}
 		em.close();
 	}
-	
-	public void changeEmployee(Employee s)
-	{
-		EntityManager em = emf.createEntityManager(); 	
+
+	public void changeEmployee(Employee s) {
+		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
 		em.merge(s);
 		em.getTransaction().commit();
 		em.close();
 	}
-	
+
+	/* Get all salary entries of an employee */
+	@SuppressWarnings("unchecked")
 	public List<Salary> getSalary(int empNo) {
 
-		// EntityManager em = emf.createEntityManager();
-		// Salary sal = em.find(Salary.class, empNo);
-		// em.close();
-		// return sal;
 		EntityManager em = emf.createEntityManager();
-		// JPQL - Java Persistence Query Language - nicht SQL
 		String sql = "SELECT s FROM Salary s WHERE s.empNo = :empNo";
 		Query query = em.createQuery(sql, Salary.class);
 		query.setParameter("empNo", empNo);
 		List<Salary> salary = null;
-		try
-		{
-		salary = (List<Salary>)query.getResultList();
-		} catch (NoResultException e)
-		{
-		e.printStackTrace();
+		try {
+			salary = (List<Salary>) query.getResultList();
+		} catch (NoResultException e) {
+			e.printStackTrace();
 		}
 		em.close();
 		return salary;
 
-		}
-	
+	}
+
 	public void close() {
 		emf.close();
 	}
--- jpa-employees/src/main/java/at/ac/htlinn/jpa/employees/Runner.java
+++ jpa-employees/src/main/java/at/ac/htlinn/jpa/employees/Runner.java
@@ -1,49 +1,72 @@
 package at.ac.htlinn.jpa.employees;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 public class Runner {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		DBManager db = new DBManager();
 		
-//		List<Employee> employees = db.getEmployees();
-//		System.out.println("Liste von Studenten:");
-//		for (Employee student : employees) {
-//			System.out.println(student);
-//		}
-//		
-//		System.out.println();
-//		System.out.println();
-//		
-//		Employee e = db.getEmployee(10001);
-//		System.out.println(e);
-//		
-//		e.setEmp_no(10000);
-//		System.out.println(e);
-		
-		Employee e = db.getEmployee(10001);
-		System.out.println(e);
-//		for (Salary s : e.getSalaries())
-//		{
-//			System.out.println(s);
-//		}
-		
-		for (DeptEmp de : e.getDeptEmp())
-		{
-			System.out.println(de);
-			System.out.println(de.getDepartment());
-		}
-		
-		for (DeptManager de : e.getDeptManager())
-		{
-			System.out.println(de.getEmployee());
-			System.out.println(de.getDepartment());
-		}
-		
-		Department d = db.getDepartment("d005");
-		for (DeptManager dm : d.getDeptManager())
-		{
-			Employee emp = dm.getEmployee();
+		List<Employee> employees = db.getEmployees();
+		System.out.println("List all Employees:");
+		for (Employee emp : employees) {
 			System.out.println(emp);
 		}
+		
+		Employee e = db.getEmployee(10001);
+		System.out.println("Get a single employee");
+		System.out.println(e);
+		
+		System.out.println();
+		List<Salary> sals = e.getSalaries();
+		System.out.println("All salary entries for this employee:");
+		for (Salary s : sals)
+		{
+			System.out.println(s);
+		}
+		
+		System.out.println();
+		System.out.println("This is the actual salary:");
+		Salary actual = db.getActualSalary(10001);
+		System.out.println(actual);
+		
+		Date d = new SimpleDateFormat( "yyyyMMdd" ).parse( "19870626" );
+		Salary s = db.getSalary(10001, d); 
+		System.out.println();
+		System.out.printf("the salary earned until %s\n",d);
+		System.out.println(s);
+		
+		System.out.println();	
+		System.out.println("Get Departments of the Employee:");
+		for (DeptEmp de : e.getDeptEmp())
+		{
+			System.out.println(de.getDepartment().getName());
+		}		
+
+		System.out.println();
+		System.out.println("Who is the boss of the employee");		
+		for (DeptEmp de : e.getDeptEmp())
+		{
+			Department dept = de.getDepartment();
+			for (DeptManager dm : dept.getDeptManager())
+			{
+				Employee boss = dm.getEmployee();
+				Date from = dm.getFromDate();
+				System.out.printf("%s %s (%s)\n", boss.getFirstname(), boss.getLastname(), from);
+			}
+		}	
+
+		System.out.println("Now we change the employee");
+		Employee e2 = db.getEmployee(10003);
+		System.out.println(e2);
+		e2.setLastname("ein neuer Name");
+		db.changeEmployee(e2);
+		
+		System.out.println();
+		System.out.println("Objects can also be removed");
+		db.removeEmployee(10003);
 		
 		db.close();
 	}
